970d62e4ccaad5b04f54a5a07fce3ba6f9add82a
consciousness_dev <consciousness.dev@gmail.com>
2022-01-06 15:12:24 +0700

    note: setup react router - with v6 (latest)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
