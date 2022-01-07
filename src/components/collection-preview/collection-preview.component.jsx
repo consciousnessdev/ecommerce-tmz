f8cf45b0009b24ca92f8eae6c1ed75b69e5a4c7d
consciousness_dev <consciousness.dev@gmail.com>
2022-01-08 04:52:44 +0700

    note: Update Shop Page - Adding collection item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,17 @@
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => {
     return (
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {/*
-                    NOTE: each Collection Preview get rendered, filter & map function also will run
-                    it cause performance issues if data is large
-                */}
                 {
                     items
                         .filter((item, idx) => idx < 4)
-                        .map(item => (
-                        <div key={item.id}>{item.name}</div>
-                    ))
+                        .map(({ id, ...otherItemProps }) => (
+                            <CollectionItem key={id} {...otherItemProps} />
+                        ))
                 }
             </div>
         </div>
