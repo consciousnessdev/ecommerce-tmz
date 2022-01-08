841bd603deb94fe6236c3eaa3279a5c0daee5fb0
consciousness_dev <consciousness.dev@gmail.com>
2022-01-08 13:13:46 +0700

    note: Create Form Input component & styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,55 +1,62 @@
 import { Component } from 'react';
 
-import "./sign-in.styles.scss";
+import './sign-in.styles.scss';
+
+import FormInput from '../form-input/form-input.component';
 
 class SignIn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-    handleSubmit = event => {
-        event.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        });
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      email: '',
+      password: '',
+    });
+  };
 
-    handleChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    }
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
-    render() {
-        const { email, password } = this.state;
-        return (
-          <div className="sign-in">
-            <h2>I already have an account</h2>
-            <span>Sign in with your email and password</span>
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="email" value={email} required onChange={this.handleChange} />
-              <label htmlFor="email">Email</label>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                required
-                onChange={this.handleChange}
-              />
-              <label htmlFor="password">Password</label>
-
-              <input type="submit" value="Submit Form" />
-            </form>
-          </div>
-        );
-    }
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="email"
+            value={email}
+            label="Email"
+            required
+            handleChange={this.handleChange}
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            label="Password"
+            required
+            handleChange={this.handleChange}
+          />
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
