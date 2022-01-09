923d8858d2e312c38e0b0dad6d018dca3f2dc2ac
consciousness_dev <consciousness.dev@gmail.com>
2022-01-09 09:28:06 +0700

    note: Store Data to Firebase - createUserProfileDocument, check current user accessing, like log

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -12,6 +12,27 @@ const config = {
   measurementId: 'G-ZD8YNY75RR',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.error('error creating user', error.message);
+    }
+  }
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
