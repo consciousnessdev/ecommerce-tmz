facbfafc25bbd0d15166358317b194ba14bd0ad6
consciousness_dev <consciousness.dev@gmail.com>
2022-01-15 21:09:12 +0700

    note: setup & implement persisting redux - persistor, persisted store & reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+// import persist reducer tool
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+// set config for persisting
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [
+    'cart',
+  ],
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+// export persisted root reducer by persistConfig
+export default persistReducer(persistConfig, rootReducer);
