e7d1a909416278b6cbac5106dfaa146bb4eb65d2
Ario Setiawan <consciousness.dev@gmail.com>
2022-01-27 06:26:04 +0700

    fix: redirect to home page even not logged in

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,26 +1,15 @@
-import { connect } from 'react-redux';
-import {Navigate} from 'react-router-dom';
-
 import './homepage.styles.scss';
 
 import { HomePageContainer } from './homepage.styles';
 
 import Directory from '../../components/directory/directory.component';
 
-const Homepage = ({ currentUser }) => {
-    if (!currentUser) {
-      return <Navigate to="/signin" replace />
-    }
+const Homepage = () => {
+  return (
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer>
+  );
+};
 
-    return (
-      <HomePageContainer>
-        <Directory />
-      </HomePageContainer>
-    );
-}
-
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
-});
-
-export default connect(mapStateToProps)(Homepage);
+export default Homepage;
