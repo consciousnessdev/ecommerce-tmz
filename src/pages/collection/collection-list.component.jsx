c8f0060c7e6188209ff8094eacc2b54891f80fd9
Ario Setiawan <consciousness.dev@gmail.com>
2022-02-01 18:00:59 +0700

    note: use container pattern for loaded collection

--- src/pages/collection/collection-list.component.jsx
+++ src/pages/collection/collection-list.component.jsx
@@ -1,14 +1,8 @@
 import { Component } from 'react';
-import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
-import CollectionComponent from './collection.component';
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
-
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-const CollectionComponentWithSpinner = WithSpinner(CollectionComponent);
+import CollectionPageContainer from './collection.container';
 
 class CollectionList extends Component {
 
@@ -18,17 +12,12 @@ class CollectionList extends Component {
   }
 
   render() {
-    const { isCollectionsLoaded } = this.props;
-    return <CollectionComponentWithSpinner isLoading={!isCollectionsLoaded} />;
+    return <CollectionPageContainer />;
   }
 }
-
-const mapStateToProps = createStructuredSelector({
-  isCollectionsLoaded: selectIsCollectionsLoaded,
-});
 
 const mapDispatchTopProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
 
-export default connect(mapStateToProps, mapDispatchTopProps)(CollectionList);
+export default connect(null, mapDispatchTopProps)(CollectionList);
