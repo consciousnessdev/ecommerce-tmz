f6eeb4960d01351c7f82a9db204c154d8197ba48
Ario Setiawan <consciousness.dev@gmail.com>
2022-02-20 06:20:50 +0700

    note: explanation of useEffect run order

--- src/pages/reacthooks/AllAboutHooks/UseEffectExample.jsx
+++ src/pages/reacthooks/AllAboutHooks/UseEffectExample.jsx
@@ -1,26 +1,32 @@
-import { useState, useEffect, useCallback, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 
 const UseEffectExample = () => {
   const [test1, setTest1] = useState(true);
-  const [test2, setTest2] = useState(true);
 
-  const myObj = useMemo(
-    () => ({
-      a: 'my value of a is ' + test1,
-    }),
-    [test1]
-  );
+  // note: useEffect run after render, in this code means after returned
+  // jsx code below rendered
+  // useEffect(() => {
+  //   console.log('effect run');
+  // },[]);
+
+  // try detect run render first
+  // console.log('render');
+
+  const consoleMessage = () => {
+    console.log('effect run');
+  }
 
   useEffect(() => {
-    console.log(myObj.a);
-  }, [myObj]);
+    consoleMessage();
+    // function as useEffect array dependencies will check is consoleMessage
+    // is difference function as previous
+  }, [consoleMessage]);
+
 
   return (
     <div>
       <h1>test1 value: {String(test1)}</h1>
-      <h1>test2 value: {String(test2)}</h1>
       <button onClick={() => setTest1(!test1)}>Flip test value</button>
-      <button onClick={() => setTest2(!test2)}>Flip test value</button>
     </div>
   );
 };
