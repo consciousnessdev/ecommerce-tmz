aba17dcac47268f31981ae87f1e72cb174675a11
Ario Setiawan <consciousness.dev@gmail.com>
2022-02-27 15:30:57 +0700

    note: build error boundary component - Example: homepage component throw error

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,6 +5,8 @@ import { HomePageContainer } from './homepage.styles';
 import Directory from '../../components/directory/directory.component';
 
 const Homepage = () => {
+  // this will throw error & error message will show
+  // throw Error;
   return (
     <HomePageContainer>
       <Directory />
